ea776527aa4ce97f82556c4ee0204eb1d619a56a
Zaki Noorani <zakinoorani2006@gmail.com>
2024-10-26 15:52:17 +0500

    .

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-import img from "./assets/img.jpg"
 
 const app = express();
 const PORT = 5000;
